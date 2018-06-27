3014f7b93a4f9d3c4a027f879935ecc372eb2b7c
Nguyen Phuc Thien An <npta97@gmail.com>
2018-06-27 17:14:22 +0700

    @ContentChild and ngAfterContentInit

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Output,
+  EventEmitter,
+  ContentChild,
+  AfterContentInit
+} from '@angular/core';
+
+import { AuthRememberComponent } from './auth-remember.component';
 
 import { User } from './auth-form.interface';
 
@@ -16,15 +24,27 @@ import { User } from './auth-form.interface';
         <input type="password" name="password" ngModel>
       </label>
       <ng-content select="auth-remember"></ng-content>
+      <div *ngIf="showMessage">
+        You will be logged in for 30 days
+      </div>
       <ng-content select="button"></ng-content>
     </form>
   `
 })
-export class AuthFormComponent {
+export class AuthFormComponent implements AfterContentInit {
   @Output() submitted: EventEmitter<User>;
+  @ContentChild(AuthRememberComponent) remember: AuthRememberComponent;
+
+  showMessage: boolean;
 
   constructor() {
     this.submitted = new EventEmitter();
+  }
+
+  ngAfterContentInit() {
+    if (this.remember) {
+      this.remember.checked.subscribe((checked: boolean) => this.showMessage = checked);
+    }
   }
 
   onSubmit(user: User) {
