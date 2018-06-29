1baad85f81726b18e11362e357cd7830b9a41010
Nguyen Phuc Thien An <npta97@gmail.com>
2018-06-29 11:49:19 +0700

    Dynamic components reordering

--- app/app.component.ts
+++ app/app.component.ts
@@ -16,9 +16,8 @@ import { User } from './auth-form/auth-form.interface';
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      <button (click)="destroyComponent()">
-        Destroy
-      </button>
+      <button (click)="destroyComponent()">Destroy</button>
+      <button (click)="moveComponent()">Move</button>
       <div #entry></div>
     </div>
   `
@@ -31,13 +30,18 @@ export class AppComponent implements AfterContentInit {
 
   ngAfterContentInit() {
     const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    this.component = this.entry.createComponent(authFormFactory);
+    this.entry.createComponent(authFormFactory);
+    this.component = this.entry.createComponent(authFormFactory, 0);
     this.component.instance.title = 'Create account';
     this.component.instance.submitted.subscribe(this.loginUser);
   }
 
   destroyComponent() {
     this.component.destroy();
+  }
+
+  moveComponent() {
+    this.entry.move(this.component.hostView, 1);
   }
 
   loginUser(user: User) {
