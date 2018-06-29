acc34de63c4f892634951764e247c99a4dad75a8
Nguyen Phuc Thien An <npta97@gmail.com>
2018-06-29 15:46:18 +0700

    Dynamic <ng-template> rendering with ngTemplateOutlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,31 +1,15 @@
-import {
-  Component,
-  AfterContentInit,
-  ViewChild,
-  ViewContainerRef,
-  TemplateRef
-} from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      <div #entry></div>
-      <template #tmpl let-name let-location="location">
-        {{name}} : {{location}}
+      <ng-container [ngTemplateOutlet]="tmpl"></ng-container>
+      <template #tmpl>
+        Ultimate Angular
       </template>
     </div>
   `
 })
-export class AppComponent implements AfterContentInit {
-  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-  @ViewChild('tmpl') tmpl: TemplateRef<any>;
-
-  ngAfterContentInit() {
-    this.entry.createEmbeddedView(this.tmpl, {
-      $implicit: 'Ultimate Angular',
-      location: 'England, UK'
-    });
-  }
-}
+export class AppComponent { }
